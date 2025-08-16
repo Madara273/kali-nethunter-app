683bec0e81261ff949d80c43896b58edfd67606b
kimocoder <christian@aircrack-ng.org>
2025-08-16 22:17:04 +0200

    CustomCommandsExecutor: align all fragments

--- src/com/offsec/nethunter/Executor/CustomCommandsExecutor.java
+++ src/com/offsec/nethunter/Executor/CustomCommandsExecutor.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 public class CustomCommandsExecutor {
 	private CustomCommandsExecutorListener listener;
 	private final int actionCode;
